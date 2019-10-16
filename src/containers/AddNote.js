94526bfaa65e089813fb8862096a2dcf9c9db36f
Eli <elijah.logan@purchase.edu>
2019-10-16 10:10:50 -0400

    where viees are held
    to add, list, delete and update notes

---
+++ src/containers/AddNote.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class AddNote extends Component {
+    render(){
+        return(
+        <div>
+            <h1>Add a note</h1>
+        </div>
+        )
+    }
+}
